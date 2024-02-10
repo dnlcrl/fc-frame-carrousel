9723c9e27313b6b28ad0b7e583195c01c3e768b8
dnlcrl <1212655+dnlcrl@users.noreply.github.com>
2024-02-10 23:13:06 +0100

    Update getNftMetadata.ts

--- app/lib/getNftMetadata.ts
+++ app/lib/getNftMetadata.ts
@@ -1,6 +1,6 @@
 export const getNftMetadata = async (nft: string, tokenId: number) => {
   const options = {method: 'GET', headers: {accept: 'application/json'}};
-  const baseUrl = 'https://base-mainnet.g.alchemy.com/';
+  const baseUrl = 'https://eth-mainnet.g.alchemy.com/';
   const endpoint = baseUrl + `nft/v3/${process.env.ALCHEMY_ID || 'docs-demo'}/getNFTMetadata`;
   const params = `?contractAddress=${nft}&tokenId=${tokenId}&refreshCache=false`;
   const response = await fetch(endpoint+params, options);
